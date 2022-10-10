a8824cd755cbecf5edc3eceffbec4247516cd06f
aldyistna <aldyistna@gmail.com>
2022-10-10 21:32:37 +0700

    update search project

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -11,17 +11,21 @@ class DownloadController extends Controller
     public $successStatus = 200;
 
     public function searchproject(){
+        $model = Project::orderBy('nama', 'asc')->limit(8)->where('flag_mcs',5);
         $cari = request()->searchTerm;
-        if ($cari == null) {
-            $hasil = Project::orderBy('nama', 'asc')->limit(8)->where('flag_mcs',5)->get();
-        }else{
+        $impl = request()->impl;
+        if ($cari != null) {
+            $model->where('nama', 'like', "%$cari%");
+//            $hasil = Project::orderBy('nama', 'asc')->limit(8)->where('flag_mcs',5)->get();
+        }/*else{
             $hasil = Project::where('nama', 'like', "%$cari%")->orderBy('nama', 'asc')->where('flag_mcs',5)->limit(8)->get();
             // $hasil = Project::where('nama', 'like', "%$cari%")->orderBy('nama', 'asc')->limit(8)->get();
-        }
+        }*/
+        $hasil = $model->get();
 
         $data = [];
         foreach ($hasil as $key) {
-            $data[]         = array("id"=>$key->id, "text"=>$key->nama, "image"=>$key->thumbnail);
+            $data[]         = array("id"=>$key->id, "text"=>$key->nama, "image"=>$key->thumbnail, "disabled" => ($impl == 'Y' && sizeof($key->implementation) > 0));
         }
 
         return json_encode($data);

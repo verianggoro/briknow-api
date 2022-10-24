ac885bd7f51db801d084982dc4e7c79050e200ae
veri <veri.anggoro@melon.co.id>
2022-10-24 15:56:02 +0700

    update api lesson learned

--- app/Http/Controllers/LessonLearnedController.php
+++ app/Http/Controllers/LessonLearnedController.php
@@ -11,30 +11,65 @@ use Illuminate\Support\Facades\Validator;
 class LessonLearnedController extends Controller
 {
     public function getAll(Request $request){
-        try{
             $validator= Validator::make($request-> all(), [
                 'tahap' => 'nullable',
+                'divisi' => 'nullable',
+                'search' => 'nullable'
             ]);
 
-            if (empty($request->tahap)){
+            if (empty($request->tahap) && empty($request->divisi) && empty($request->search)){
                 $query = Project::with(['lesson_learned'])->get();
-            }else{
+            }elseif (!empty($request->tahap) && empty($request->divisi) && empty($request->search)){
                 $tahp = $request->tahap;
                 $query = Project::whereHas('lesson_learned', function ($q) use ($tahp) {
                     $q->where('tahap', '=', $tahp);
                 })->get();
+            }elseif (empty($request->tahap) && !empty($request->divisi) && empty($request->search)){
+                $div = $request->divisi;
+                $query = Project::whereHas('lesson_learned', function ($q) use ($div) {
+                    $q->where('divisi_id', '=', $div);
+                })->get();
+            }elseif(empty($request->tahap) && empty($request->divisi) && !empty($request->search)){
+                $key = $request->search;
+                $query = Project::whereHas('lesson_learned', function ($q) use ( $key) {
+                    $q->where('lesson_learned', 'LIKE', '%'.$key.'%');
+                })->get();
+            }elseif (!empty($request->tahap) && !empty($request->divisi) && empty($request->search)){
+                $div = $request->divisi;
+                $tahp = $request->tahap;
+                $query = Project::whereHas('lesson_learned', function ($q) use ($div, $tahp) {
+                    $q->where('divisi_id', '=', $div);
+                    $q->where('tahap', '=', $tahp);
+                })->get();
+            }elseif (!empty($request->tahap) && empty($request->divisi) && !empty($request->search)){
+                $tahp = $request->tahap;
+                $key = $request->search;
+                $query = Project::whereHas('lesson_learned', function ($q) use ($tahp, $key) {
+                    $q->where('tahap', '=', $tahp);
+                    $q->where('lesson_learned', 'LIKE', '%'.$key.'%');
+                })->get();
+            }elseif (empty($request->tahap) && !empty($request->divisi) && !empty($request->search)){
+                $div = $request->divisi;
+                $key = $request->search;
+                $query = Project::whereHas('lesson_learned', function ($q) use ($div, $key) {
+                    $q->where('divisi_id', '=', $div);
+                    $q->where('lesson_learned', 'LIKE', '%'.$key.'%');
+                })->get();
+            }elseif (!empty($request->tahap) && !empty($request->divisi) && !empty($request->search)){
+                $div = $request->divisi;
+                $key = $request->search;
+                $tahp = $request->tahap;
+                $query = Project::whereHas('lesson_learned', function ($q) use ($div, $key, $tahp) {
+                    $q->where('divisi_id', '=', $div);
+                    $q->where('lesson_learned', 'LIKE', '%'.$key.'%');
+                    $q->where('tahap', '=', $tahp);
+                })->get();
             }
+
             return response()->json([
                 "message"   => "GET Berhasil",
                 "status"    => 1,
                 "data"      => $query
             ],200);
-        }catch (\Throwable $th){
-            $datas['message']    =   'GET Gagal';
-            return response()->json([
-                'status'    =>  0,
-                'data'      =>  $datas
-            ],200);
-        }
     }
 }
